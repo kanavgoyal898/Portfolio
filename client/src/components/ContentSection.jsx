e48a4f8c324e82a594720a7da8ab0437221e486d
Kanav Goyal <kanavgoyal898@gmail.com>
2025-01-10 05:48:06 +0530

    Fix Bugs

--- client/src/components/ContentSection.jsx
+++ client/src/components/ContentSection.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from 'react';
+import LogoSection from './LogoSection'
 import './ContentSection.css'
 
 const ContentSection = ({ navigationItems }) => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsVisible(window.innerWidth < 768)
+        }
+
+        handleResize()
+
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
     return (
         <div className="content-container">
+            {isVisible && <LogoSection />}
             {navigationItems.map((navigationItem, index) => (
                 <div key={ index } id={ navigationItem.id } className="content-section">
                     { navigationItem.componentName }
