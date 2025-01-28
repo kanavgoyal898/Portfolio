0c7904b4624ef70f92f51c3c780ea442f5fa193e
Kanav Goyal <kanavgoyal898@gmail.com>
2025-01-28 19:33:45 +0530

    Improve UI/UX

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,19 +1,13 @@
 import { useState, useEffect } from "react";
 import "./Gallery.css";
 
-const Work = () => {
+const Gallery = () => {
   const [pictures, setPictures] = useState([]);
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/pictures`)
       .then((response) => response.json())
       .then((data) => setPictures(data.sort((a, b) => a.priority - b.priority)))
       .catch((error) => console.error("Error fetching images:", error));
-  }, []);
-
-  const [height, setHeight] = useState(0);
-  useEffect(() => {
-    setHeight(window.innerHeight);
-    window.addEventListener("resize", () => setWidth(window.innerHeight));
   }, []);
     
   return (
@@ -29,4 +23,4 @@ const Work = () => {
   );
 };
 
-export default Work;
+export default Gallery;
